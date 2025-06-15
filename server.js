a73453887d61c43e874e3205b8ce8147a575acc4
Shariarhosain <shariarhosain131529@gmail.com>
2025-06-15 21:53:42 +0600

    six docker and server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { upload, getFileUrl, deleteFile, UPLOAD_DIR } from './upload.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3200;
 
 // --- CORS and JSON middleware ---
 app.use(express.json());
